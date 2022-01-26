9bdc20c7998f6ab236f38dd67a75cb7d7cd5ce5c
Nidhin <myofficialskknidhin@gmail.com>
2022-01-26 12:06:09 +0530

    Source commit
    This is the source code for FreeRTOS on TM4C123GH6PM Cortex M4F microcontroller.

---
+++ TM4C123GH6PM_CM4/keil/main.c
@@ -0,0 +1,46 @@
+#include <stdint.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
+/* Declare a variable that will be incremented by the hook function. */
+volatile uint32_t ulIdleCycleCount = 0UL;
+
+void vPeriodicTask(void *pvParameters);
+
+int main()
+{
+	xTaskCreate(vPeriodicTask, "My Task", 256, NULL, 1, NULL);
+
+	// Startup of the FreeRTOS scheduler.  The program should block here.  
+	vTaskStartScheduler();
+	
+	// The following line should never be reached.  Failure to allocate enough
+	//	memory from the heap would be one reason.
+	for (;;);
+	
+}
+
+void vPeriodicTask(void *pvParameters)
+{
+	
+	// Establish the task's period....
+	const TickType_t xDelay = pdMS_TO_TICKS(1000);
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	
+	for (;;) {
+
+		
+		// Block until the next release time.
+		vTaskDelayUntil(&xLastWakeTime, xDelay);
+	}
+
+}	
+
+/* Idle hook functions MUST be called vApplicationIdleHook(), take no parameters,
+and return void. */
+void vApplicationIdleHook( void )
+{
+ /* This hook function does nothing but increment a counter. */
+ulIdleCycleCount++;
+
+}
